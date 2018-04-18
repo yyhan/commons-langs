2eeb89f2328e6f1d3ee8890db93e9e023c1f4c4c
小天 <cloudin@foxmail.com>
2018-04-18 22:45:01 +0800

    设置 Charsets 类过期

--- src/main/java/com/cloudin/commons/langs/Charsets.java
+++ src/main/java/com/cloudin/commons/langs/Charsets.java
@@ -6,7 +6,9 @@ import java.nio.charset.Charset;
  *
  * @author 小天
  * @version 1.0.0, 2017/10/25 0025 15:51
+ * @deprecated 请使用 {@link java.nio.charset.StandardCharsets}
  */
+@Deprecated
 public class Charsets {
 	
 	public static final Charset GBK   = Charset.forName("GBK");
