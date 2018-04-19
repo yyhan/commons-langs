228e99b87690396760865c06874f5d7cfcafa3bb
小天 <cloudin@foxmail.com>
2018-04-19 22:01:31 +0800

    使用 StandardCharsets 替换 Charsets 类

--- src/main/java/com/cloudin/commons/langs/ContentTypes.java
+++ src/main/java/com/cloudin/commons/langs/ContentTypes.java
@@ -1,48 +1,47 @@
 package com.cloudin.commons.langs;
 
-import org.apache.http.Consts;
 import org.apache.http.entity.ContentType;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
- *
  * @author 小天
  * @version 1.0.0, 2017/10/25 0025 15:51
  */
 public class ContentTypes {
-	
-	/**
-	 * 将form表单转换为 urlencoded 处理后的键值对
-	 */
-	public static final ContentType APPLICATION_FORM_URLENCODED_UTF8 = ContentType.create("application/x-www-form-urlencoded", Charsets.UTF_8);
-	/**
-	 * json
-	 */
-	public static final ContentType APPLICATION_JSON_UTF8            = ContentType.create("application/json", Consts.UTF_8);
-	/**
-	 * xml
-	 */
-	public static final ContentType APPLICATION_XML_UTF8             = ContentType.create("application/xml", Charsets.UTF_8);
-	/**
-	 *
-	 */
-	public static final ContentType MULTIPART_FORM_DATA_UTF8         = ContentType.create("multipart/form-data", Charsets.UTF_8);
-	/**
-	 * html
-	 */
-	public static final ContentType TEXT_HTML_UTF8                   = ContentType.create("text/html", Charsets.UTF_8);
-	/**
-	 * 明文文本
-	 */
-	public static final ContentType TEXT_PLAIN_UTF8                  = ContentType.create("text/plain", Charsets.UTF_8);
-	/**
-	 * xml文本
-	 */
-	public static final ContentType TEXT_XML_UTF8                   = ContentType.create("text/xml", Charsets.UTF_8);
-	/**
-	 * 通配符
-	 */
-	public static final ContentType WILDCARD                    = ContentType.create("*/*", (Charset) null);
-	
+
+    /**
+     * 将form表单转换为 urlencoded 处理后的键值对
+     */
+    public static final ContentType APPLICATION_FORM_URLENCODED_UTF8 = ContentType.create("application/x-www-form-urlencoded", StandardCharsets.UTF_8);
+    /**
+     * json
+     */
+    public static final ContentType APPLICATION_JSON_UTF8 = ContentType.create("application/json", StandardCharsets.UTF_8);
+    /**
+     * xml
+     */
+    public static final ContentType APPLICATION_XML_UTF8 = ContentType.create("application/xml", StandardCharsets.UTF_8);
+    /**
+     *
+     */
+    public static final ContentType MULTIPART_FORM_DATA_UTF8 = ContentType.create("multipart/form-data", StandardCharsets.UTF_8);
+    /**
+     * html
+     */
+    public static final ContentType TEXT_HTML_UTF8 = ContentType.create("text/html", StandardCharsets.UTF_8);
+    /**
+     * 明文文本
+     */
+    public static final ContentType TEXT_PLAIN_UTF8 = ContentType.create("text/plain", StandardCharsets.UTF_8);
+    /**
+     * xml文本
+     */
+    public static final ContentType TEXT_XML_UTF8 = ContentType.create("text/xml", StandardCharsets.UTF_8);
+    /**
+     * 通配符
+     */
+    public static final ContentType WILDCARD = ContentType.create("*/*", (Charset) null);
+
 }
--- src/main/java/com/cloudin/commons/langs/URLCodec.java
+++ src/main/java/com/cloudin/commons/langs/URLCodec.java
@@ -1,9 +1,13 @@
 package com.cloudin.commons.langs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 提供 <CODE>application/x-www-form-urlencoded</CODE> 标准的字符串编解码工具
@@ -12,30 +16,32 @@ import java.nio.charset.Charset;
  * @version 1.0.0, 2017/10/25 0025 15:51
  */
 public class URLCodec {
+
+	private static Logger logger = LoggerFactory.getLogger(URLCodec.class);
 	
 	public static String encode(String plainText) {
-		return encode(plainText, Charsets.UTF_8);
+		return encode(plainText, StandardCharsets.UTF_8);
 	}
 	
 	public static String encode(String plainText, Charset charset) {
 		try {
 			return URLEncoder.encode(plainText, charset.displayName());
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			logger.error("", e);
 		}
 		return plainText;
 	}
 	
 	
 	public static String decode(String plainText) {
-		return decode(plainText, Charsets.UTF_8);
+		return decode(plainText, StandardCharsets.UTF_8);
 	}
 	
 	public static String decode(String plainText, Charset charset) {
 		try {
 			return URLDecoder.decode(plainText, charset.displayName());
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			logger.error("", e);
 		}
 		return plainText;
 	}
